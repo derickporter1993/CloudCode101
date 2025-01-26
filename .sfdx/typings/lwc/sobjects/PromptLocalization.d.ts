9156421c3f3786fe4d87aa7674984185a4bc55c0
derickporter1993 <145419370+derickporter1993@users.noreply.github.com>
2025-01-25 20:29:17 -0800

    Main

---
+++ .sfdx/typings/lwc/sobjects/PromptLocalization.d.ts
@@ -0,0 +1,52 @@
+declare module "@salesforce/schema/PromptLocalization.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/PromptLocalization.Parent" {
+  const Parent:any;
+  export default Parent;
+}
+declare module "@salesforce/schema/PromptLocalization.ParentId" {
+  const ParentId:any;
+  export default ParentId;
+}
+declare module "@salesforce/schema/PromptLocalization.Language" {
+  const Language:string;
+  export default Language;
+}
+declare module "@salesforce/schema/PromptLocalization.Value" {
+  const Value:string;
+  export default Value;
+}
+declare module "@salesforce/schema/PromptLocalization.NamespacePrefix" {
+  const NamespacePrefix:string;
+  export default NamespacePrefix;
+}
+declare module "@salesforce/schema/PromptLocalization.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/PromptLocalization.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/PromptLocalization.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/PromptLocalization.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/PromptLocalization.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/PromptLocalization.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/PromptLocalization.SystemModstamp" {
+  const SystemModstamp:any;
+  export default SystemModstamp;
+}
---
+++ .sfdx/typings/lwc/sobjects/PromptVersionLocalization.d.ts
@@ -0,0 +1,52 @@
+declare module "@salesforce/schema/PromptVersionLocalization.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/PromptVersionLocalization.Parent" {
+  const Parent:any;
+  export default Parent;
+}
+declare module "@salesforce/schema/PromptVersionLocalization.ParentId" {
+  const ParentId:any;
+  export default ParentId;
+}
+declare module "@salesforce/schema/PromptVersionLocalization.Language" {
+  const Language:string;
+  export default Language;
+}
+declare module "@salesforce/schema/PromptVersionLocalization.Value" {
+  const Value:string;
+  export default Value;
+}
+declare module "@salesforce/schema/PromptVersionLocalization.NamespacePrefix" {
+  const NamespacePrefix:string;
+  export default NamespacePrefix;
+}
+declare module "@salesforce/schema/PromptVersionLocalization.CreatedDate" {
+  const CreatedDate:any;
+  export default CreatedDate;
+}
+declare module "@salesforce/schema/PromptVersionLocalization.CreatedBy" {
+  const CreatedBy:any;
+  export default CreatedBy;
+}
+declare module "@salesforce/schema/PromptVersionLocalization.CreatedById" {
+  const CreatedById:any;
+  export default CreatedById;
+}
+declare module "@salesforce/schema/PromptVersionLocalization.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/PromptVersionLocalization.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/PromptVersionLocalization.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/PromptVersionLocalization.SystemModstamp" {
+  const SystemModstamp:any;
+  export default SystemModstamp;
+}
